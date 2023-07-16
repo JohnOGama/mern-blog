07c43163310079db450826c6231a67013bdddd1c
John De Leon <112483991+JohnOGama@users.noreply.github.com>
2023-07-17 00:24:23 +0800

    Update Login.jsx

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const login = async (e) => {
     e.preventDefault();
-    const response = await fetch("mern-blog-api-bay.vercel.app/login", {
+    const response = await fetch("https://mern-blog-api-bay.vercel.app/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
